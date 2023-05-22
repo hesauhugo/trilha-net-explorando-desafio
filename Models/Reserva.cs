c8e1a175d38c66323e88af46fe7788b1834964d0
hesauhugo <hesau.hugo@hotmail.com>
2023-05-22 16:38:57 -0300

    feat: valida hospedes

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -40,7 +40,7 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
             // *IMPLEMENTE AQUI*
-            return 0;
+            return Hospedes.Count;
         }
 
         public decimal CalcularValorDiaria()
@@ -48,13 +48,13 @@ namespace DesafioProjetoHospedagem.Models
             // TODO: Retorna o valor da diária
             // Cálculo: DiasReservados X Suite.ValorDiaria
             // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+            decimal valor = (decimal)DiasReservados*Suite.ValorDiaria;
 
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
             // *IMPLEMENTE AQUI*
-            if (true)
+            if (DiasReservados>=10)
             {
-                valor = 0;
+                valor = valor*0.9M;
             }
 
             return valor;
