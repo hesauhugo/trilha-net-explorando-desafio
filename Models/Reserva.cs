5cc1703b7e30b8d22e25b2a2fda5b188e4efc5d2
hesauhugo <hesau.hugo@hotmail.com>
2023-05-22 16:41:13 -0300

    feat: quantidade de hospedes e desconto

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -18,15 +18,13 @@ namespace DesafioProjetoHospedagem.Models
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
-            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
+
             if (hospedes.Count<=Suite.Capacidade)
             {
                 Hospedes = hospedes;
             }
             else
             {
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
                 throw new InvalidOperationException("Quantidade de hospedes supera a capacidade");
             }
         }
@@ -38,20 +36,15 @@ namespace DesafioProjetoHospedagem.Models
 
         public int ObterQuantidadeHospedes()
         {
-            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
+
             return Hospedes.Count;
         }
 
         public decimal CalcularValorDiaria()
         {
-            // TODO: Retorna o valor da diária
-            // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
+
             decimal valor = (decimal)DiasReservados*Suite.ValorDiaria;
 
-            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
             if (DiasReservados>=10)
             {
                 valor = valor*0.9M;
